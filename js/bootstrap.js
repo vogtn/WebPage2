f41d82f9c0eda502d50b170b04e3dd36fd09fc36
vogtn <vogtn@uw.edu>
2016-09-02 23:20:22 -0700

    adding fade after timeout

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,6 +3,9 @@
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
+ setTimeout(function() {
+    $('#h1').addClass('fadeIn');
+}, 4000);
 
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
